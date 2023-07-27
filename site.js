dd646c96889b6c0fbcb8c59c546f68d3460c57c6
Jake Bladt <jake@jakebladt.com>
2023-07-27 17:44:06 -0400

    Call Lambda function from static site on CloudFront

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    let fetchPromise = fetch('https://jsonplaceholder.typicode.com/todos/2');
+    let fetchPromise = fetch('https://xpxhz7ljec7doxp5x66ejkemiu0glcaz.lambda-url.us-east-1.on.aws/');
     fetchPromise
         .then(response => response.json())
-        .then(data => document.getElementById('taskParagraph').innerHTML = data.title);
+        .then(data => document.getElementById('timestampSpan').innerHTML = data.timestamp);
 }
